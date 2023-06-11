390f0ca13d1b4f4d01586ebf42c397c5aaeee553
Alexander Atamanyuk <psotiframe13@gmail.com>
2023-06-11 19:26:16 +0500

    add gulpfile settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 // Gulpfile
 
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const { src, dest, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
 
-exports.default = defaultTask;
+const sassCompile = (done) => {
+  console.log('Compile SASS to CSS');
+
+  return src('app/sass/*.scss')
+    .pipe(sass())
+    .pipe(dest('build/styles/'));
+};
+
+const pugCompile = (done) => {
+  console.log('Compile Pug to HTML');
+
+  return src('app/pages/*.pug')
+    .pipe(pug())
+    .pipe(dest('build/'));
+};
+
+exports.default = series(sassCompile, pugCompile);
