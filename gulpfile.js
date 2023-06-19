bfdd7d2e5fdb49fe4577a8d1fc19af0d2ab7fb65
Alexander Atamanyuk <psotiframe13@gmail.com>
2023-06-19 16:57:22 +0500

    add browsersync and watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 // Gulpfile
 
-const { src, dest, series } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+const browserSyncJob = () => {
+  browserSync.init({
+    server: "build/"
+  });
+
+  watch('app/sass/**/*.scss', sassCompile);
+  watch('app/pages/**/*.pug', pugCompile);
+};
 
 const sassCompile = (done) => {
   console.log('Compile SASS to CSS');
 
   return src('app/sass/app.scss')
     .pipe(sass())
-    .pipe(dest('build/styles/'));
+    .pipe(dest('build/styles/'))
+    .pipe(browserSync.stream());
 };
 
 const pugCompile = (done) => {
@@ -17,7 +28,9 @@ const pugCompile = (done) => {
 
   return src('app/pages/*.pug')
     .pipe(pug())
-    .pipe(dest('build/'));
+    .pipe(dest('build/'))
+    .pipe(browserSync.stream());
 };
 
 exports.default = series(sassCompile, pugCompile);
+exports.server = browserSyncJob;
