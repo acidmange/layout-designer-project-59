4847b0b9b32519a00b076f48342b41989edd9bfe
Alexander Atamanyuk <psotiframe13@gmail.com>
2023-09-10 21:24:35 +0500

    add new files from previous repository and build

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const { watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
 
 const browserSyncJob = () => {
   browserSync.init({
@@ -17,30 +18,45 @@ const browserSyncJob = () => {
 const buildSass = () => {
   console.log('Компиляция SASS');
 
-  return src('src/sass/*.scss')
+  return src('app/sass/*.scss')
     .pipe(sass())
     .pipe(dest('build/styles/'))
     .pipe(browserSync.stream());
 }
 
+const buildSVG = () => {
+  console.log('Компиляция SVG');
+
+  return src('./build/images/icons/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../sprite.svg"
+        }
+      },
+    }
+    ))
+    .pipe(dest('build/svg/'))
+};
+
 const buildPug = () => {
     console.log('Компиляция Pug');
   
-    return src('src/pages/*.pug')
+    return src('app/pages/*.pug')
       .pipe(pug({ pretty: true }))
       .pipe(dest('build/'))
       .pipe(browserSync.stream());
 }
 
 const destImages = () => {
-  return src('src/images/**/*')
+  return src('app/images/**/*')
     .pipe(dest('build/images'));
 }
 
 const destJs = () => {
-  return src('src/bootstrap/dist/js/bootstrap.min.js')
+  return src('app/bootstrap/dist/js/bootstrap.min.js')
     .pipe(dest('build/js/'));
 }
 
 exports.server = browserSyncJob;
-exports.build = parallel(buildSass, buildPug, destImages, destJs);
+exports.build = parallel(buildSass, buildPug, buildSVG, destImages, destJs);
